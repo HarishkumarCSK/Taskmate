1b39a3fe78e1206e58a48290cf7ef89d3e8ba2d8
HarishkumarCSK <erty457@gmail.com>
2022-02-04 09:58:48 -0800

    initial commit

---
+++ todolist_app/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from todolist_app.models import Tasklist
+from todolist_app.forms import Taskform
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def todolist(request):
+    if request.method =="POST":
+        form=Taskform(request.POST or None)
+        if form.is_valid():
+            form.save(commit=False).manage=request.user
+            form.save()
+        messages.success(request,("new task added!"))
+        return redirect('todolist')
+        
+    else:
+        all_tasks=Tasklist.objects.filter(manage=request.user)
+        paginator=Paginator(all_tasks,5)
+        page=request.GET.get('pg')
+        all_tasks=paginator.get_page(page)
+    
+        return render(request,'todolist.html',{'all_tasks':all_tasks})
+@login_required   
+def delete_task(request,task_id):
+    task=Tasklist.objects.get(pk=task_id)
+    if task.manage==request.user:
+        task.delete()
+        
+    else:
+        messages.error(request,("Access restricted,you are not allowed!"))
+    return redirect('todolist')
+@login_required
+def edit_task(request,task_id):
+    if request.method =="POST":
+        task=Tasklist.objects.get(pk=task_id)
+        form=Taskform(request.POST or None , instance=task)
+        if form.is_valid():
+            form.save()
+        
+        messages.success(request,(" task edited!"))
+        return redirect('todolist')
+        
+    else:
+        task_obj=Tasklist.objects.get(pk=task_id)
+        
+    
+        return render(request,'edit.html',{'task_obj':task_obj})
+@login_required    
+def complete_task(request,task_id):
+    task=Tasklist.objects.get(pk=task_id)
+    if task.manage==request.user:
+       task.done=True
+       task.save()
+    else:
+        messages.error(request,("Access restricted,you are not allowed!"))
+    return redirect('todolist')
+@login_required 
+def pending_task(request,task_id):
+    task=Tasklist.objects.get(pk=task_id)
+    if task.manage==request.user:
+       task.done=False
+       task.save()
+    
+    else:
+          messages.error(request,("Access restricted,you are not allowed!"))
+    return redirect('todolist')
+ 
+
+def contact(request):
+    context={
+        'contact_text':'welcome to contact page'
+    }
+    return render(request,'contact.html',context)
+
+def about(request):
+    context={
+        'about_text':'welcome to about page'
+    }
+    return render(request,'about.html',context)
+
+def index(request):
+    context={
+        'index_text':'welcome to index page'
+    }
+    return render(request,'index.html',context)
